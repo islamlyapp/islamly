62355388f0f4bea0fe8ef4a1c56de2e4bb2036e4
islamly <islamlystaff@gmail.com>
2026-03-02 11:29:39 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7199214099-199f4",
+  "appId": "1:848547570312:web:ba33f46b93a313e1f4753d",
+  "apiKey": "AIzaSyCvuTpCB-nr2yRgsr9VTFNhjTMzGfZwipw",
+  "authDomain": "studio-7199214099-199f4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "848547570312"
+};
